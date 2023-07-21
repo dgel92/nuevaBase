4d1a726519c8e1baaeba675fcbd4112c70b91138
dgel92 <dgel92@gmail.com>
2023-07-21 00:36:47 -0300

    21-07-23 02

---
+++ src/componentes/Data/Data.js
@@ -0,0 +1,36 @@
+const Itrackers =[
+    {
+        id:1,
+        name: "Servicio Reducido Por Mora sin saldo pendiente",
+        Etiqueta: "FAN",
+        CBS: false,
+        InfoAlCliente: "Informarle al cliente que en el transcurso de 3hrs a 72hrs se va a dar resolucion a su caso.",
+        Base:"https://basedeconocimientos.custhelp.com/app/answers/detail/a_id/10701/kw/servicio%20reducido",
+        usuario:"u",
+        cuenta: "N° de cuenta",
+        detalle:"Detalle",
+        mail: "Mail",
+        telContacto: "Telefono de contacto",
+        captura: "Captura de resumen de cuenta y vista 360"
+    },
+    {
+        id:2,
+        name: "Instalacion o service con cambio de modem o decos con orden en espera de ejecucion",
+        Etiqueta: "FAN",
+        CBS: false,
+        InfoAlCliente: "Informarle al cliente que por inconvenientes en las herramientas no se pudo cerrar su orden de instalacion, por este motivo no podemos ver sus equipos y no podemos hacer diagnostico tecnico. Que ya esta generado el reclamo y que en el transcurso de 03 a 72hrs va a estar activo su servicio",
+        Base:"-",
+        usuario:"u",
+        cuenta: "N° de cuenta",
+        caso: "Caso",
+        ot: "orden tecnica",
+        domicilio: "direccion (Loc, prov, partido)",
+        detalle:"Detalle",
+        mail: "Mail",
+        telContacto: "Telefono de contacto",
+        captura: "Captura del caso en espera de ejecucion"
+        
+    }
+]
+
+export default dataArray
---
+++ src/componentes/ModoOscuro/ModoOscuro.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ModoOscuro(){
+    return(
+        <div></div>
+    )
+}
+export default ModoOscuro
